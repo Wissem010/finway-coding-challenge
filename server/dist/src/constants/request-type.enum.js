cc2a190e06faeec78fa10adc62ac78bf62c7d6be
unknown <nasralllahwissem@gmail.com>
2022-12-15 22:45:05 +0100

    fix: removed dist files

--- server/dist/src/constants/request-type.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENUM_REQUEST_TYPE = void 0;
-var ENUM_REQUEST_TYPE;
-(function (ENUM_REQUEST_TYPE) {
-    ENUM_REQUEST_TYPE["CALCULATE"] = "CALCULATE";
-    ENUM_REQUEST_TYPE["HISTORY"] = "HISTORY";
-    ENUM_REQUEST_TYPE["ONBOARDING"] = "ONBOARDING";
-})(ENUM_REQUEST_TYPE = exports.ENUM_REQUEST_TYPE || (exports.ENUM_REQUEST_TYPE = {}));
